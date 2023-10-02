6c5f7ad6cd390a5752d8550ef1dcda7365a9977a
imartin-latency <124692644+imartin-latency@users.noreply.github.com>
2023-10-02 13:12:11 +0200

    Add files via upload

---
+++ support/constatns.py
@@ -0,0 +1,17 @@
+"""
+
+Constants.py 
+
+This modulde contains constant information for the system. 
+Any constant shall be included in capital Case and with a brief
+explainatory comment
+
+
+"""
+
+
+
+# Example of the name of this project
+NAME_PROJECT = 'Automatic processing and consolidation of ESG information based on open web sources'
+
+
